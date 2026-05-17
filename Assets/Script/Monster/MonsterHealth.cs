7e0509592cadbd368e1c33aae4a07b650eecb1de
yeonjoon <kyjyeonjoon@gmail.com>
2026-05-17 12:34:09 +0900

    몬스터 체력 UI 구현 후 스크립트와 연결

--- Assets/Script/Monster/MonsterHealth.cs
+++ Assets/Script/Monster/MonsterHealth.cs
@@ -1,23 +1,35 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MonsterHealth : MonoBehaviour
 {
     public int maxHp = 100;
     private int currentHp;
 
+    public Slider monsterHpSlider;
+
     void Start()
     {
         currentHp = maxHp;
+
+        if (monsterHpSlider != null)
+        {
+            monsterHpSlider.maxValue = maxHp;
+            monsterHpSlider.value = currentHp;
+        }
     }
 
     public void TakeDamage(int damage)
     {
         currentHp -= damage;
-        Debug.Log("몬스터 피격! 남은 HP: " + currentHp);
+
+        if (monsterHpSlider != null)
+        {
+            monsterHpSlider.value = currentHp;
+        }
 
         if (currentHp <= 0)
         {
-            Debug.Log("몬스터 사망");
             Destroy(gameObject);
         }
     }
