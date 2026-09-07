88bb1c4fdd9989662fcd75e627dc6ca47a2aecad
linksp12 <127829547+linksp12@users.noreply.github.com>
2026-09-07 22:58:45 +0900

    체력수치 조정
    - 체력수치 15

--- Assets/Script/Ui/GameManager.cs
+++ Assets/Script/Ui/GameManager.cs
@@ -15,7 +15,7 @@ public class GameManager : MonoBehaviour
     private int currentMoney;
 
     [Header("Player HP")]
-    public int maxPlayerHp => 100;
+    public int maxPlayerHp => 15;
     private int currentPlayerHp;
     public TextMeshProUGUI hpText;
     public Image damageImage;
